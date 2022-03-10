03d722a7b04846c355ff24d626d00813320f58f5
CanabarroMatheus <matheus.canabarro@dbccompany.com.br>
2022-03-10 20:21:35 -0300

    Add list contact-type resource.

--- coworking/coworking-project/src/main/java/io/github/canabarromatheus/coworkingsystem/rest/resource/ClientResource.java
+++ coworking/coworking-project/src/main/java/io/github/canabarromatheus/coworkingsystem/rest/resource/ClientResource.java
@@ -2,35 +2,41 @@ package io.github.canabarromatheus.coworkingsystem.rest.resource;
 
 import io.github.canabarromatheus.coworkingsystem.exception.InvalidCPFException;
 import io.github.canabarromatheus.coworkingsystem.rest.dto.request.ClientRequest;
-import io.github.canabarromatheus.coworkingsystem.rest.dto.response.ClientResponse;
 import io.github.canabarromatheus.coworkingsystem.rest.service.ClientService;
+import io.github.canabarromatheus.coworkingsystem.rest.service.ContactTypeService;
 
 import javax.inject.Inject;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-import java.util.List;
 
 @Path("/v1/clients")
 @Consumes(MediaType.APPLICATION_JSON)
 @Produces(MediaType.APPLICATION_JSON)
 public class ClientResource {
-    ClientService service;
+    ClientService clientService;
+    ContactTypeService contactTypeService;
 
     @Inject
-    public ClientResource(ClientService service) {
-        this.service = service;
+    public ClientResource(ClientService clientService, ContactTypeService contactTypeService) {
+        this.clientService = clientService;
+        this.contactTypeService = contactTypeService;
     }
 
     @POST
     public Response addClient(ClientRequest request) throws InvalidCPFException {
-        service.save(request);
+        clientService.save(request);
         return Response.status(201).build();
     }
 
     @GET
     public Response listClients() {
-        List<ClientResponse> responses = service.findAll();
-        return Response.ok(responses).build();
+        return Response.ok(clientService.findAll()).build();
+    }
+
+    @GET
+    @Path("/contact-types")
+    public Response listContactTypes() {
+        return Response.ok(contactTypeService.findAll()).build();
     }
 }
