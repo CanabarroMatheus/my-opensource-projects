e036436894dfe55ee32a5f520bf9daef9d048f79
matheus_canabarro <matheus_canabarro@sicredi.com.br>
2022-06-17 16:48:28 -0300

    add(*): First commit.

---
+++ balance-system/balance-system/src/test/java/canabarro/matheus/balancesystem/BalanceSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package canabarro.matheus.balancesystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BalanceSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
